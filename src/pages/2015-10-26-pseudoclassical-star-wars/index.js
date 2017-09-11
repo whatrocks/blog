15871f4c0a88c0e597b41ab63a984d7aea6c0c1f
Charlie Harrington <charlie@whatrocks.org>
2017-09-10 18:07:53 -0700

    finish star wars post by removing animations :(

--- src/pages/2015-10-26-pseudoclassical-star-wars/index.js
+++ src/pages/2015-10-26-pseudoclassical-star-wars/index.js
@@ -26,33 +26,13 @@ export default function Post({ pathContext }) {
 
 // This is where you write the post!
 class CustomPost extends Component {
-  state = {
-    stars: [],
-    twinkleStars: []
-  }
-
-  componentWillMount() {
-    let newStars = [];
-    let twinkleStars = [];
-    for (var i = 0; i < 20; i++) {
-      var star = { x: Math.random() * 700, y: (Math.random() * 400), timeBetweenSteps: 200 }
-      newStars.push(star);
-      twinkleStars.push(star);
-    }
-    this.setState({ stars: newStars, twinkleStars: twinkleStars });
-  }
-
-
 
   render() {
-    const { images } = this.props;
-    const { stars, twinkleStars } = this.state;
     return (
       <div>
         <p>Subclassing in JavaScript is quite useful. I used JavaScript subclasses to create this Star Wars inspired game.</p>
         <img alt="aluminum-falcon" src="img/subclasswars.gif" />
         <p>You can <a href="http://whatrocks.github.io/aluminum-falcon">play it right now</a>, or <a href="https://github.com/whatrocks/subclasswars.js">check out the code</a> on Github. The rest of this insanely long post will demonstrate how to use JavaScript subclassing to build the foundations of a similar game or visualization.</p>
-        <p><i>Another note - when I originally wrote this post I was using the Pelican static site generator and wrote these interactive examples in jQuery. I've recently moved to a Gatsby/React static site, so I had to rewrite these visualizations in React. But the jQuery code blocks should still work as good examples of what's going on here.</i></p>
         <h3>Subclassing overview</h3>
         <p>A class can be written to create fleets of similar objects with shared properties and methods. For example, if you're building the game Frogger, you can create a class called Vehicle that will produce a single instance of one of those pesky little cars. Let's imagine that your Vehicle class has one property (location, to track a car's current location) and one method (move, to drive a car forward). Based on the difficulty of the game level, you can use your Vehicle class to produce as many cars as you want to hinder that frog from its goal.</p>
         <p>But what if you get the idea to introduce a new type of vehicle that nefariously changes lanes at random, or a friendly car that scoops up the froggie and brings him to the pond? You could certainly add those features to your original Vehicle class, but then all instances of Vehicle would have those new properties and methods, and it may prove difficult to track and turn them on for certain Vehicle instances based on your game design (e.g. produce one and only one friendly car per level).</p>
@@ -70,21 +50,7 @@ class CustomPost extends Component {
           </pre>
         </code>
 
-        <div style={{
-          "height": 400,
-          "width": 700,
-          "backgroundColor": "black",
-          "position": "relative"
-        }}>
-          {
-            stars &&
-            stars.map((star, index) => {
-              return <Star key={index} x={star.x} y={star.y} timeBetweenSteps={star.timeBetweenSteps} />
-            })
-          }
-        </div>
-
-        <p>Hello, stars!! Note that I've used the pseudoclassical instantiation pattern (rather than the functional or prototypal patterns) to build the Star class, so new stars need to be instantiated with the "new" keyword. Another important detail for this and any other HTML visualizations is that the origin (the [0,0] location) of any HTML element or document is the upper left corner.</p>
+        <p>Hello, stars! Note that I've used the pseudoclassical instantiation pattern (rather than the functional or prototypal patterns) to build the Star class, so new stars need to be instantiated with the "new" keyword. Another important detail for this and any other HTML visualizations is that the origin (the [0,0] location) of any HTML element or document is the upper left corner.</p>
         <p>Now, let's make those stars twinkle to show you how subclassing works. We will need to refactor our base Star class slightly:</p>
 
         <code>
@@ -93,21 +59,7 @@ class CustomPost extends Component {
           </pre>
         </code>
 
-        <div style={{
-          "height": 400,
-          "width": 700,
-          "backgroundColor": "black",
-          "position": "relative"
-        }}>
-          {
-            twinkleStars &&
-            twinkleStars.map((star, index) => {
-              return <TwinkleStar key={index} x={star.x} y={star.y} timeBetweenSteps={star.timeBetweenSteps} />
-            })
-          }
-        </div>
-
-        <p>Wow! Now we're getting somewhere. Reload the page if you want another random astrology sign constellation.</p>
+        <p>Now we're getting somewhere!</p>
         <p>A lot happened in the last in the last iteration of code, and it's important to highlight some things now. First, we introduced a new method to our base Star class called 'step' that essentially mimics a game loop with the native JS function setTimeout. This is a little hacky and definitely not what you want to use for a real game, but this is just a demo, and hey, it's me.</p>
         <img alt="stars" src="img/hey.gif" />
         <p>With our new step method built into the base Star class, we can now create lots of different subclasses of Star that add unique functionality. TwinkleStar twinkles, but we might want other stars that don't twinkle. How about stars that move instead of twinkle? Good idea, but we're not ready to get there just yet.</p>
@@ -115,8 +67,8 @@ class CustomPost extends Component {
         <p>Let's talk about how we created the TwinkleStar subclass. We created a function called TwinkleStar with the same parameter signature as Star. Within that function, the Star class function is called using the .call method to bind 'this' to the new TwinkleStar object being created. Now, the new TwinkleStar will be set up with the base properties of a regular ol' Star (e.g. x, y, timeBetweenSteps, $node). Outside of the TwinkleStar function, we set up property and method delegation to the Star class using the Object.create pattern so that our new TwinkleStars can use any of the methods from the Star class (e.g. step, setPosition). Then we changed the constructor property to the TwinkleStar class so that any new TwinkleStars will be able to state that they are an "instanceof" TwinkleStar (and not Star). Finally, the coolest thing we do is write a new step method for TwinkleStar where we specify our unique action for all TwinkleStars. First we call the original Star step method, and then, crucially, we use jQuery to toggle the star span node on and off each time step gets executed - AKA making them twinkle.</p>
         <p>That's it! JavaScript subclassing in the pseudoclassical pattern.</p>
         <h3>The lost city of Z</h3>
-        <p>Now it's time to get serious about our screensaver replica. We need to make moving stars. But not just any moving stars, we need our stars to move in such a way that it feels like we are being whisked away to a faraway planet for a new and exciting adventure. The stars need to move "towards" the screen, even though we can only place these HTML spans on a flat 2D surface. Yikes. Now, there are lots of really impressive implementations of moving starfields on the web, so it's definitely possible, but will require some math. After some searching, I discovered this post which provides a very simple procedure that can calculate an x and y position for a star taking into account a simulated z-axis property.</p>
-        <p>Okay, that looks awesome. We did it!</p>
+        <p>Now it's time to get serious about our screensaver replica. We need to make moving stars. But not just any moving stars, we need our stars to move in such a way that it feels like we are being whisked away to a faraway planet for a new and exciting adventure. The stars need to move "towards" the screen, even though we can only place these HTML spans on a flat 2D surface. Yikes. Now, there are lots of really impressive implementations of moving starfields on the web, so it's definitely possible, but will require some math. After some searching, I discovered a very simple procedure that can calculate an x and y position for a star taking into account a simulated z-axis property.</p>
+
         <p>I'll walk through our changes in the code. Note that I'm not including the code for the Star or TwinkleStar classes since those won't be changing anymore.</p>
         <code>
           <pre>
@@ -133,10 +85,10 @@ class CustomPost extends Component {
           </pre>
         </code>
 
-        <p>Okay, that worked, sort of. Simply by replacing the type of DOM element with an img tag, we've turned our MoveStars into Tiefighters! But there's clearly some kind of CSS border situation going on that we'll have to investigate. And the TieFighters are not disappearing immediately when they hit the bottom or right side of the div. They're probably flying right over this very text as we speak. But they are disappearing correctly when they hit the left or top. Strange. However, if you recall that the origin of an HTML element is the upper left hand corner of its rectangle, then this bug makes more sense. This issue wouldn't have been obvious if this were a full screen animation. In order to resolve, we would have to overwrite some of MoveStar's calculations around resetting the image location to also account for the size of the img.</p>
+        <p>Okay, that worked, sort of. Simply by replacing the type of DOM element with an img tag, we've turned our MoveStars into Tiefighters! But there's clearly some kind of CSS border situation going on that we'll have to investigate. And the TieFighters are not disappearing immediately when they hit the bottom or right side of the div. But they are disappearing correctly when they hit the left or top. Strange. However, if you recall that the origin of an HTML element is the upper left hand corner of its rectangle, then this bug makes more sense. In order to resolve, we would have to overwrite some of MoveStar's calculations around resetting the image location to also account for the size of the img.</p>
         <p>And then there's this horrible border thing. The border is definitely the result of setting the CSS properties for the Star and MoveStar classes directly in the JavaScript rather than in a separate CSS class that we can toggle on or off. If we move the styling of the Star and MoveStar (specifically the border-style, border-color, and border-radius) to a separate CSS file, then we wouldn't have these white borders on our TieFighters. For now, let's just pretend these boxes are from the Millenium Falcon's weapons targetting system.</p>
         <h3>The Force Awakens</h3>
-        <p>At this point, we've now demonstrated how to use JavaScript subclassing to create the foundations of a Stars Wars or Star Trek type game. A big Chewbacca-style hug to Tate Thurston who was my co-conspirator on this project.</p>
+        <p>At this point, we've now demonstrated how to use JavaScript subclassing to create the foundations of a Stars Wars or Star Trek type game. A big Chewbacca-style hug to <a href="https://github.com/tatethurston" alt="Tate Thurston">Tate Thurston</a> who was my co-conspirator on this project.</p>
         <p>Rather than continue to walkthrough the code (because this post is long enough already), I'll instead list some considerations, challenges, and steps we faced in the transition from this visualization to our game:</p>
         <h4>Millenium Falcon cockpit</h4>
         <ul>
@@ -150,11 +102,11 @@ class CustomPost extends Component {
           <li>Set an onClick listener to track succesful "hits", and then turn the Tiefighter img source into an explosion-type gif, and then remove the destroyed TieFighter. (Sidenote - this is more challenging than simply removing the DOM node, since the TieFighter object will continue to exist in memory even though the DOM node was removed. Frustratingly, we kept getting invisible "hits" from already-destroyed TieFighters, and we finally resolved by using a boolean flag to track whether a Tiefighter had been destroyed or not before iterating to the next call to the original Star "step" function).</li>
         </ul>
         <h4>Sounds</h4>
-        <ul><li>Use this free website to create laser, explosion, injury, and other fun videogame sounds, and then trigger them via jQuery event listeners</li>
+        <ul><li>Use one of the many free sound effects websites to create laser, explosion, injury, and other fun videogame sounds, and then trigger them via event listeners</li>
           <li>Find some background music and set on autoplay with an HTML audio tag</li>
         </ul>
         <h4>Hyperspace</h4>
-        <p>If you've already tried the game demo, you may have noticed that the Millenium Falcon's hyperdrive is not operational. That's an obvious next step for future development of SubClassWars.js - dive in and help!</p>
+        <p>If you've already tried the game demo, you may have noticed that the Millenium Falcon's hyperdrive is not operational. That's an obvious next step for future development!</p>
       </div>
     );
   }
@@ -457,59 +409,3 @@ const code4 =
    this.$node.css({height: size, width: size});
    MoveStar.prototype.step.call(this);
  };`
-
-class Star extends Component {
-  state = {
-    x: '',
-    y: '',
-    timeBetweenSteps: ''
-  }
-
-  componentWillMount() {
-    this.setState(this.props);
-  }
-
-  render() {
-    const { x, y, timeBetweenSteps } = this.props;
-    return <span style={{ top: y, left: x, border: "2px", borderStyle: "solid", borderColor: "white", borderRadius: "2px", position: "absolute" }} />
-  }
-
-}
-
-class TwinkleStar extends Component {
-  state = {
-    x: '',
-    y: '',
-    timeBetweenSteps: '',
-    timer: null,
-    display: 'inline'
-  }
-
-  componentDidMount() {
-    const { x, y, timeBetweenSteps } = this.props;
-    this.step();
-    this.setState({ x: x, y: y, timeBetweenSteps: timeBetweenSteps });
-  }
-
-  step() {
-    const { timeBetweenSteps, display } = this.state;
-    let newDisplay;
-    let timer = setTimeout(() => {
-      newDisplay = (display === "inline") ? "none" : "inline";
-      this.setState({ display: newDisplay });
-      this.step();
-    }, timeBetweenSteps);
-    this.setState({ timer: timer });
-  }
-
-  componentWillUnmount() {
-    const { timer } = this.state;
-    clearInterval(timer);
-  }
-
-  render() {
-    const { x, y, timeBetweenSteps, display } = this.state;
-    return <span style={{ top: y, left: x, border: "2px", borderStyle: "solid", borderColor: "white", borderRadius: "2px", position: "absolute", display: display }} />
-  }
-
-}

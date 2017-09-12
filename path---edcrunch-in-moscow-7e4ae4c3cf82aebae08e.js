775e7f9476ed7e703a4c5e3dee44fc80942bf07d
Charlie Harrington <charlie@whatrocks.org>
2017-09-11 21:11:25 -0700

    Updates

---
+++ path---edcrunch-in-moscow-7e4ae4c3cf82aebae08e.js
@@ -0,0 +1,2 @@
+webpackJsonp([39263181206136],{"./node_modules/json-loader/index.js!./.cache/json/edcrunch-in-moscow.json":function(e,o){e.exports={data:{markdownRemark:{html:'<iframe width="560" height="315" src="https://www.youtube.com/embed/T952UorytGs?rel=0" frameborder="0" allowfullscreen></iframe>',frontmatter:{date:"October 18, 2014",path:"/edcrunch-in-moscow",title:"edCrunch in Moscow",author:"Charlie Harrington",category:"Talks"}}},pathContext:{}}}});
+//# sourceMappingURL=path---edcrunch-in-moscow-7e4ae4c3cf82aebae08e.js.map

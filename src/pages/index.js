fb25af7da06197e50551e39670fdaf38cee7dc30
Charlie Harrington <charlie@whatrocks.org>
2017-09-17 08:22:18 -0700

    fix index gif

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,101 +31,105 @@ export default function Index({ data }) {
     let newPost = post;
     newPost.node.indexImage =
       imageMap[
-      `${post.node.frontmatter.fullPath}/images/${post.node.frontmatter.image}`
+        `${post.node.frontmatter.fullPath}/images/${post.node.frontmatter
+          .image}`
       ];
     return newPost;
   });
   return (
     <div>
-      <div style={{ borderBottom: "4px solid green", width: "200px", margin: "0 auto" }}>
+      <div
+        style={{
+          borderBottom: "4px solid green",
+          width: "200px",
+          margin: "0 auto"
+        }}
+      >
         <h4 style={{ textAlign: "center" }}>Latest posts</h4>
       </div>
       {posts
         .filter(post => post.node.frontmatter.title.length > 0)
         .map(({ node: post }, index) => {
           return (
-            post.frontmatter.path !== "/react-post" &&
-            <div
-              key={index}
-              style={{
-                marginTop: "2rem",
-                marginBottom: "2rem",
-                backgroundColor: "white",
-                display: "flex",
-                flexWrap: "wrap",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-              className="post"
-            >
-              <Link
-                to={post.frontmatter.path}
-                className="indexLink"
+            post.frontmatter.path !== "/react-post" && (
+              <div
+                key={index}
                 style={{
-                  border: "2px solid rgb(238, 238, 238)",
-                  padding: "2rem",
+                  marginTop: "2rem",
+                  marginBottom: "2rem",
+                  backgroundColor: "white",
                   display: "flex",
-                  flexDirection: "column",
+                  flexWrap: "wrap",
+                  flexDirection: "row",
                   justifyContent: "center",
-                  alignItems: "center",
-                }}>
-                <h3 style={{ textAlign: "center", marginBottom: "0.5rem" }}>
-                  {post.frontmatter.title}
-                </h3>
-                <h5 style={{ textAlign: "center" }}>
-                  {moment(new Date(post.frontmatter.date)).format("DD MMMM YYYY")}
-                </h5>
-                {
-                  post.frontmatter.title === "Pseudoclassical Star Wars"
-                  &&
-                  <img
-                    style={{
-                      maxHeight: "450px",
-                      marginTop: "1rem",
-                      marginBottom: "0",
-                    }}
-                    src={"img/subclasswars.gif"}
-                  />
-                }
-                {
-                  post.frontmatter.title === "Space Time Travel with Wormie"
-                  &&
-                  <img
-                    style={{
-                      maxHeight: "450px",
-                      marginTop: "1rem",
-                      marginBottom: "0",
-                    }}
-                    src={"img/WormieLive.gif"}
-                  />
-                }
-                {
-                  post.frontmatter.title === "Meet Von Count"
-                  &&
-                  <img
-                    style={{
-                      maxHeight: "450px",
-                      marginTop: "1rem",
-                      marginBottom: "0",
-                    }}
-                    src={"img/vc.gif"}
-                  />
-                }
-                {
-                  post.indexImage
-                  &&
-                  <img
-                    style={{
-                      maxHeight: "450px",
-                      marginTop: "1rem",
-                      marginBottom: "0",
-                    }}
-                    src={post.indexImage}
-                  />
-                }
-              </Link>
-            </div>
+                  alignItems: "center"
+                }}
+                className="post"
+              >
+                <Link
+                  to={post.frontmatter.path}
+                  className="indexLink"
+                  style={{
+                    border: "2px solid rgb(238, 238, 238)",
+                    padding: "2rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center"
+                  }}
+                >
+                  <h3 style={{ textAlign: "center", marginBottom: "0.5rem" }}>
+                    {post.frontmatter.title}
+                  </h3>
+                  <h5 style={{ textAlign: "center" }}>
+                    {moment(new Date(post.frontmatter.date)).format(
+                      "DD MMMM YYYY"
+                    )}
+                  </h5>
+                  {post.frontmatter.title === "Pseudoclassical Star Wars" && (
+                    <img
+                      style={{
+                        maxHeight: "450px",
+                        marginTop: "1rem",
+                        marginBottom: "0"
+                      }}
+                      src={"img/subclasswars.gif"}
+                    />
+                  )}
+                  {post.frontmatter.title ===
+                    "Space Time Travel with Wormie" && (
+                    <img
+                      style={{
+                        maxHeight: "450px",
+                        marginTop: "1rem",
+                        marginBottom: "0"
+                      }}
+                      src={"img/WormieLive.gif"}
+                    />
+                  )}
+                  {post.frontmatter.title === "Meet Von Chomp" && (
+                    <img
+                      style={{
+                        maxHeight: "450px",
+                        marginTop: "1rem",
+                        marginBottom: "0"
+                      }}
+                      src={"img/vc.gif"}
+                    />
+                  )}
+                  {post.indexImage && (
+                    <img
+                      style={{
+                        maxHeight: "450px",
+                        marginTop: "1rem",
+                        marginBottom: "0"
+                      }}
+                      src={post.indexImage}
+                    />
+                  )}
+                </Link>
+              </div>
+            )
           );
         })}
     </div>

fe473fcbda4499b7f8ebd061abddffd50452dfff
Charlie Harrington <charles.harrington@gmail.com>
2018-11-11 17:14:31 -0800

    sidebar styles

---
+++ src/layouts/subscribe.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default () => (
+  <form
+    style={{
+      border: "1px solid rgb(238, 238, 238)",
+      borderRadius: "6px",
+      padding: "3px",
+      backgroundColor: "grey",
+      textAlign: "center"
+    }}
+    action="https://tinyletter.com/whatrocks"
+    method="post"
+    target="popupwindow"
+    onSubmit={() => {
+      window.open(
+        "https://tinyletter.com/whatrocks",
+        "popupwindow",
+        "scrollbars=yes,width=800,height=600"
+      );
+      return true;
+    }}
+  >
+    <p
+      style={{
+        marginTop: "1rem",
+        fontSize: "1rem"
+      }}
+    >
+      <label for="tlemail">
+        <span role="img" aria-label="wave">
+          👋{" "}
+        </span>
+        Join my almost-never newsletter for updates on projects, writing,
+        mascots.
+      </label>
+    </p>
+    <p>
+      <input
+        type="text"
+        placeholder="ripley@weyland-yutani.com"
+        style={{
+          padding: "0.5rem",
+          minWidth: "240px"
+        }}
+        name="email"
+        id="tlemail"
+      />
+    </p>
+    <input type="hidden" value="1" name="embed" />
+    <input type="submit" value="Subscribe" />
+    <p
+      style={{
+        fontSize: "0.75rem"
+      }}
+    >
+      <a
+        href="https://tinyletter.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        powered by TinyLetter
+      </a>
+    </p>
+  </form>
+);

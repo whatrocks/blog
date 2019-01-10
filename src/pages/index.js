180816a7cc4e8f57bcfb5429947f889c9c6181a3
Charlie Harrington <charles.harrington@gmail.com>
2019-01-09 21:53:57 -0800

    update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
-import Layout from '../layouts';
+import Layout from "../layouts";
+import s from "./index.module.scss";
 
 export default function Index({ data }) {
   const { edges: mdPosts } = data.allMarkdownRemark;
   const { edges: jsPosts } = data.allJavascriptFrontmatter;
-  let frontmatteredJsPosts = jsPosts.filter(post => post.node.frontmatter.title);
+  let frontmatteredJsPosts = jsPosts.filter(
+    post => post.node.frontmatter.title
+  );
   const sortedPosts = mdPosts.concat(frontmatteredJsPosts).sort((a, b) => {
     return (
       new Date(b.node.frontmatter.date) - new Date(a.node.frontmatter.date)
@@ -13,26 +16,22 @@ export default function Index({ data }) {
   });
   return (
     <Layout>
+      <div className={s.index}>
         {sortedPosts
           .filter(post => post.node.frontmatter.title.length > 0)
           .map(({ node: post }, index) => {
             return (
               post.frontmatter.path !== "/react-post" && (
-                <div key={index} style={{ marginBottom: "0.75rem" }}>
+                <div key={index} className={s.blogLink}>
                   <Link to={post.frontmatter.path} className="indexLink">
-                    <div>
-                      <span style={{ fontSize: "1.2rem" }}>
-                        {post.frontmatter.title}
-                      </span>
-                    </div>
-                    <div style={{ fontSize: "1rem" }}>
-                      {post.frontmatter.date}
-                    </div>
+                    <span className={s.title}>{post.frontmatter.title}</span>
+                    <span className={s.date}>{post.frontmatter.date}</span>
                   </Link>
                 </div>
               )
             );
           })}
+      </div>
     </Layout>
   );
 }
@@ -55,7 +54,9 @@ export const pageQuery = graphql`
         }
       }
     }
-    allJavascriptFrontmatter(sort: { order: DESC, fields: [frontmatter___date] }) {
+    allJavascriptFrontmatter(
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
       edges {
         node {
           id

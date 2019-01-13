8f76a8917968b4dec7bf19fed95dc237ae0ed61a
Charlie Harrington <charles.harrington@gmail.com>
2019-01-13 12:29:55 -0800

    fix library mobile view

--- src/pages/library.js
+++ src/pages/library.js
@@ -9,7 +9,39 @@ export default function Library({ data }) {
   return (
     <Layout>
       <h2>Library</h2>
-      <table>
+      <div className={s.cards}>
+        {books.map((book, i) => {
+          return (
+            <div className={s.card} key={i}>
+              <div className={s.date}>
+                {new Date(book.date_added).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "long",
+                  year: "numeric"
+                })}
+              </div>
+              <img
+                alt={book.book.title_without_series}
+                className={s.bookCover}
+                src={book.book.image_url}
+              />
+              <div className={s.title}>
+                <a href={book.link} rel="noopener noreferrer" target="_blank">
+                  {book.book.title_without_series}
+                </a>
+              </div>
+              <div>
+                {book.book.authors.map((author, j) => {
+                  return <div key={j}>{author.name}</div>;
+                })}
+              </div>
+              <div>{book.book.publisher}</div>
+              {book.body && <div>{book.body}</div>}
+            </div>
+          );
+        })}
+      </div>
+      <table className={s.table}>
         <thead>
           <tr>
             <th>Cover</th>
@@ -17,7 +49,6 @@ export default function Library({ data }) {
             <th>Author(s)</th>
             <th>Date Read</th>
             <th>Publisher</th>
-            <th>Published</th>
             <th>My review</th>
           </tr>
         </thead>
@@ -50,7 +81,6 @@ export default function Library({ data }) {
                   })}
                 </td>
                 <td>{book.book.publisher}</td>
-                <td>{book.book.publication_year}</td>
                 {book.body && <td>{book.body}</td>}
               </tr>
             );
@@ -62,7 +92,7 @@ export default function Library({ data }) {
         <span role="img" aria-label="books">
           📚
         </span>{" "}
-        My thanks to the good ol' Goodreads API for this tasty data o' mine.
+        My thanks to the good ol' Goodreads API for this tasty data.
       </p>
     </Layout>
   );
@@ -84,7 +114,6 @@ export const pageQuery = graphql`
               id
               title_without_series
               image_url
-              publication_year
               publisher
               authors {
                 name

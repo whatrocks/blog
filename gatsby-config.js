0a5aedd66fd2fb31a438f0d76cef7eef2cdef7f2
Charlie Harrington <charles.harrington@gmail.com>
2020-03-14 11:00:37 -0700

    fix rss links

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,11 +27,63 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-feed`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.note.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.nodes.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+             {
+               allMarkdownRemark(
+                 sort: { order: DESC, fields: [frontmatter___date] },
+               ) {
+                 edges {
+                   node {
+                     excerpt
+                     html
+                     fields { slug }
+                     frontmatter {
+                       title
+                       date
+                     }
+                   }
+                 }
+               }
+             }
+            `,
+            output: "/rss.xml",
+            title: "Charlie Harrington's RSS Feed",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {

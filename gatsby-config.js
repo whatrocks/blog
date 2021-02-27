35e9eff0903116f66a825835fa3e333bb3551023
Charlie Harrington <charles.harrington@gmail.com>
2021-02-27 15:35:29 -0800

    redo library without goodreads

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,10 +112,21 @@ module.exports = {
       },
     },
     {
-      resolve: `@jamesdanylik/gatsby-source-goodreads`,
+      resolve: `gatsby-source-google-spreadsheets`,
       options: {
-        key: process.env.GOODREADS_API_KEY,
-        id: process.env.GOODREADS_USER_ID
+        spreadsheetId: `${process.env.GOOGLE_SPREADSHEET_ID}`,
+        credentials: {
+          type: 'service_account',
+          project_id: process.env.GOOGLE_PROJECT_ID,
+          private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+          private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
+          client_email: process.env.GOOGLE_CLIENT_EMAIL,
+          client_id: '',
+          auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+          token_uri: 'https://oauth2.googleapis.com/token',
+          auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+          client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.GOOGLE_PROJECT_ID}%40appspot.gserviceaccount.com`,
+        }, 
       }
     },
     {
